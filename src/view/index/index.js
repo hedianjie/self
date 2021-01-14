2897920754a1ee534ea9a1a04ebf925d77129803
hedianjie <1163486362@qq.com>
2021-01-14 16:42:08 +0800

    s

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -2,9 +2,41 @@ import '../../public/css/public.css'
 import './index.css'
 import $ from 'jquery'
 
-$.ajax({
-    url: '/user',
-    success(result) {
-        console.log(result)
-    }
-})
+//登录
+// $.ajax({
+//     type: 'post',
+//     url: '/login',
+//     data: {
+//         login_name: 'hedianjie2112.',
+//         pwd: '123321'
+//     },  
+//     success(result) {
+        
+//     }
+// })
+
+// 注册
+// $.ajax({
+//     type: 'post',
+//     url: '/login/register',
+//     data: {
+//         login_name: 'hedianjie2112.',
+//         pwd: '123321'
+//     },  
+//     success(result) {
+        
+//     }
+// })
+
+// 用户信息
+// $.ajax({
+//     type: 'get',
+//     url: '/user',
+//     // data: {
+//     //     login_name: 'hedianjie2112.',
+//     //     pwd: '123321'
+//     // },  
+//     success(result) {
+        
+//     }
+// })
